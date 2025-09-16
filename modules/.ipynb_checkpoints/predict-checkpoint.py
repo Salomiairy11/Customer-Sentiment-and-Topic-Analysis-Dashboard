6b585cd473815a7c3507c54429be0b4a5c5869ce
Salomi Airy <airysalomi@gmail.com>
2025-09-16 21:01:23 +0545

    Remove ignored files from repo

--- modules/.ipynb_checkpoints/predict-checkpoint.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import joblib
-
-def predict_sentiment(data, review_col='Full_Review'):
-    model = joblib.load("./models/model.pkl")
-    preds = model.predict(data[review_col])
-    labels = {0: 'Negative', 1: 'Neutral', 2: 'Positive'}
-    data['Sentiment'] = [labels[p] if p in labels else 'Unknown' for p in preds]
-    return data
